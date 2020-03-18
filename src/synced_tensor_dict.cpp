eb0fe3609f910528ec5e13141d62555c6182932f
cyy <cyyever@outlook.com>
2020-03-18 18:32:57 +0800

    add code

--- src/synced_tensor_dict.cpp
+++ src/synced_tensor_dict.cpp
@@ -4,9 +4,10 @@ namespace cyy::pytorch {
   synced_tensor_dict::synced_tensor_dict(const std::string &storage_dir_)
       : storage_dir(storage_dir_) {
 
-    if (storage_dir.exists()) {
-      if (!storage_dir.is_dir()) {
-        throw std::invalid_argument(storage_dir + " is not a directory");
+    if (std::filesystem::exists(storage_dir)) {
+      if (!std::filesystem::is_directory(storage_dir)) {
+        throw std::invalid_argument(storage_dir.string() +
+                                    " is not a directory");
       }
       for (const auto &f : std::filesystem::directory_iterator(storage_dir)) {
         py::str key = f.path().c_str();
@@ -14,6 +15,8 @@ namespace cyy::pytorch {
       }
     }
   }
+
+  void synced_tensor_dict::release() {}
 
   torch::Tensor synced_tensor_dict::get(const py::object &key) const {
     auto it = data_info.find(key);
@@ -23,39 +26,53 @@ namespace cyy::pytorch {
     auto const &[state, it2] = it->second;
     if (state == data_state::IN_MEMORY ||
         state == data_state::IN_MEMORY_NEW_DATA) {
-      return *it2;
+      return it2->second;
     }
     // TODO
     throw "unaaa";
   }
   void synced_tensor_dict::set(const py::object &key,
                                const torch::Tensor &value) {
-    auto node = data_info.extra(key);
+    auto node = data_info.extract(key);
     if (node.empty()) {
       auto it = data.emplace(data.end(), key, value);
-      data_info.emplace(key, {data_state::IN_MEMORY_NEW_DATA, it});
+      data_info.emplace(key, decltype(data_info)::mapped_type{
+                                 data_state::IN_MEMORY_NEW_DATA, it});
       return;
     }
-    auto &[_, it2] = node.value();
-    *it2 = value;
-    update_access_order(it2);
+    auto &[_, it2] = node.mapped();
+    it2->second = value;
+    it2 = update_access_order(it2);
     data_info.insert(std::move(node));
   }
   void synced_tensor_dict::remove(const py::object &key) {
-    auto node = data_info.extra(key);
+    auto node = data_info.extract(key);
     if (node.empty()) {
       throw py::key_error(py::str(key));
     }
-    auto const &[state, it2] = node.value();
+    auto const &[state, it2] = node.mapped();
     if (it2 != data.end()) {
       data.erase(it2);
     }
   }
-  void update_access_order(decltype(data::iterator) it) {
+  synced_tensor_dict::tensor_list_type::iterator
+  synced_tensor_dict::update_access_order(tensor_list_type::iterator it) {
+    auto new_it = data.insert(data.end(), std::move(*it));
     data.erase(it);
-
-
-
+    return new_it;
   }
 
+  bool synced_tensor_dict::change_state(const py::object &key,
+                                        data_state old_state,
+                                        data_state new_state) {
+    auto it = data_info.find(key);
+    if (it == data_info.end()) {
+      return false;
+    }
+    if (it->second.first != old_state) {
+      return false;
+    }
+    it->second.first = new_state;
+    return true;
+  }
 } // namespace cyy::pytorch
--- src/synced_tensor_dict.hpp
+++ src/synced_tensor_dict.hpp
@@ -1,7 +1,8 @@
 #include <filesystem>
-#include <torch/extension.h>
 #include <tuple>
 #include <unordered_map>
+
+#include <torch/extension.h>
 
 #include "lib/runnable.hpp"
 
@@ -14,16 +15,12 @@ namespace std {
 namespace cyy::pytorch {
   class synced_tensor_dict final {
   public:
-    using data_iterator_type=decltype(data)::iterator;
+    using tensor_list_type = std::list<std::pair<py::object, torch::Tensor>>;
     explicit synced_tensor_dict(const std::string &storage_dir_);
-
+    void release();
     void set(const py::object &key, const torch::Tensor &value);
     torch::Tensor get(const py::object &key) const;
     void remove(const py::object &key);
-
-  private:
-    void set_in_disk(const py::object &key);
-    void update_access_order(decltype(data::iterator) it);
 
   private:
     enum class data_state {
@@ -37,18 +34,26 @@ namespace cyy::pytorch {
     };
 
   private:
+    void set_in_disk(const py::object &key);
+    tensor_list_type::iterator
+    update_access_order(tensor_list_type::iterator it);
+    bool change_state(const py::object &key, data_state old_state,
+                      data_state new_state);
+
+  private:
     std::filesystem::path storage_dir;
-    std::list<std::pair<py::object, torch::Tensor>> data;
+    tensor_list_type data;
     std::unordered_map<py::object,
-                       std::tuple<data_state, decltype(data::iterator)>>
+                       std::tuple<data_state, tensor_list_type::iterator>>
         data_info;
+    bool permenant{false};
   };
 } // namespace cyy::pytorch
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   auto sub_m = m.def_submodule("data_structure", "Contains data structures");
   py::class_<cyy::pytorch::synced_tensor_dict>(sub_m, "SyncedTensorDict")
-      .def(py::init<>())
+      .def(py::init<const std::string &>())
       .def("__setitem__", &cyy::pytorch::synced_tensor_dict::set)
       .def("__getitem__", &cyy::pytorch::synced_tensor_dict::get)
       .def("__delitem__", &cyy::pytorch::synced_tensor_dict::remove);
