df64d3bdc8f375f49ebb5563095fc8a7d33a1f25
cyy <cyyever@outlook.com>
2020-11-23 17:47:31 +0800

    fix tests

--- test/test_synced_sparse_tensor_dict.py
+++ test/test_synced_sparse_tensor_dict.py
@@ -1,16 +1,17 @@
 import torch
 import cyy_pytorch_cpp
 
-tensor = torch.eye(3)
-tensor_dict = cyy_pytorch_cpp.data_structure.SyncedSparseTensorDict(
-    tensor.to_sparse(), tensor.shape, ""
-)
-tensor_dict.set_in_memory_number(10)
-tensor_dict.set_storage_dir("tensor_dict_dir")
-for i in range(10):
-    tensor_dict[str(i)] = tensor
+def test_synced_sparse_tensor_dict():
+    tensor = torch.eye(3)
+    tensor_dict = cyy_pytorch_cpp.data_structure.SyncedSparseTensorDict(
+        tensor.to_sparse(), tensor.shape, ""
+    )
+    tensor_dict.set_in_memory_number(10)
+    tensor_dict.set_storage_dir("tensor_dict_dir")
+    for i in range(10):
+        tensor_dict[str(i)] = tensor
 
-tensor_dict.prefetch([str(i) for i in range(100)])
-assert str(0) in tensor_dict
-for i in tensor_dict.keys():
-    assert torch.sum(torch.eq(tensor_dict[i], tensor) == False) == 0
+    tensor_dict.prefetch([str(i) for i in range(100)])
+    assert str(0) in tensor_dict
+    for i in tensor_dict.keys():
+        assert torch.sum(torch.eq(tensor_dict[i], tensor) == False) == 0
--- test/test_synced_tensor_dict.py
+++ test/test_synced_tensor_dict.py
@@ -1,14 +1,16 @@
 import torch
 import cyy_pytorch_cpp
 
-tensor_dict = cyy_pytorch_cpp.data_structure.SyncedTensorDict("")
-tensor_dict.set_in_memory_number(10)
-tensor_dict.set_storage_dir("tensor_dict_dir")
-# tensor_dict.set_permanent_storage()
-for i in range(100):
-    tensor_dict[str(i)] = torch.Tensor([i])
 
-tensor_dict.prefetch([str(i) for i in range(100)])
-assert str(0) in tensor_dict
-for i in tensor_dict.keys():
-    assert tensor_dict[i] == torch.Tensor([int(i)])
+def test_synced_tensor_dict():
+    tensor_dict = cyy_pytorch_cpp.data_structure.SyncedTensorDict("")
+    tensor_dict.set_in_memory_number(10)
+    tensor_dict.set_storage_dir("tensor_dict_dir")
+    # tensor_dict.set_permanent_storage()
+    for i in range(100):
+        tensor_dict[str(i)] = torch.Tensor([i])
+
+    tensor_dict.prefetch([str(i) for i in range(100)])
+    assert str(0) in tensor_dict
+    for i in tensor_dict.keys():
+        assert tensor_dict[i] == torch.Tensor([int(i)])
